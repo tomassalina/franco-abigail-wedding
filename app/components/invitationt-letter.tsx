21453d3c935bc3dc3e941149153d239ebea9a422
Tomas Salina <salinatomass53@gmail.com>
2025-05-14 09:56:50 -0300

    refactor(invitation-letter): improve the letter button (cta) for looking like the design

--- app/components/invitationt-letter.tsx
+++ app/components/invitationt-letter.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface InvitationLetterProps {
   handleOpen: () => void;
 }
@@ -21,10 +23,18 @@ export default function InvitationLetter({
 
       <button
         onClick={handleOpen}
-        className="absolute border-0 outline-0 cursor-pointer bg-secondary tracking-normal leading-none w-16 h-16 md:w-24 md:h-24 rounded-full text-white"
+        className="absolute border-0 outline-0 cursor-pointer bg-secondary tracking-normal leading-none w-18 h-18 md:w-24 md:h-24 rounded-full text-white flex flex-col gap-1 items-center justify-center"
       >
-        <p className="text-xl md:text-3xl">F&A</p>
-        <span className="text-xs md:text-sm">Abrir</span>
+        <div className="md:pr-[2px] pt-3">
+          <Image
+            src="/icons/logo.icon[mini].svg"
+            alt="Fran and Abigail logo mini"
+            width={60}
+            height={23}
+            className="w-12 h-4 md:w-[60px] md:h-[23px] object-contain"
+          />
+        </div>
+        <span className="text-xs md:text-sm uppercase">Abrir</span>
       </button>
     </div>
   );
